7efe1e5412c6f322d1b4ce3704ebb080447a12fc
dhruv2494 <bhuvadhruv24@gmail.com>
2024-02-23 12:46:02 +0530

    Securety

--- src/firebase/Firebase.js
+++ src/firebase/Firebase.js
@@ -1,14 +1,15 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getFirestore, collection } from "firebase/firestore";
+
 const firebaseConfig = {
-  apiKey: "AIzaSyDJ-8-OVrE_SBP0raxcTrnDt3lzAl9gbdU",
-  authDomain: "dhruv-22740.firebaseapp.com",
-  projectId: "dhruv-22740",
-  storageBucket: "dhruv-22740.appspot.com",
-  messagingSenderId: "767161074727",
-  appId: "1:767161074727:web:a91f0a3d1c596f252c3a02",
-  measurementId: "G-1XJ0B73621",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
